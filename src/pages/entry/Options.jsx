56b3b0a622100ff3c9c7cd90ccc1cbf2ac80cc9a
Sai Harshith <saiharshith987@gmail.com>
2023-08-05 21:13:26 +0530

    Added error handlers and tests

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -2,17 +2,27 @@ import React, { useEffect, useState } from "react";
 import ScoopOptions from "./ScoopOptions";
 import { Row } from "react-bootstrap";
 import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
 
 function Options({ optionType }) {
   const [data, setData] = useState();
+  const [error, setError] = useState(false);
   //optionType can only be scoops or toppings
 
   useEffect(() => {
     const getOptions = async () => {
-      const response = await fetch(`http://localhost:3030/${optionType}`);
-      const options = await response.json();
-      console.log(options);
-      setData(options);
+      await fetch(`http://localhost:3030/${optionType}`)
+        .then((res) => {
+          return res.json();
+        })
+        .then((options) => {
+          console.log(options);
+          setData(options);
+        })
+        .catch((err) => {
+          console.log(err);
+          setError(true);
+        });
     };
     getOptions();
   }, [optionType]);
@@ -26,6 +36,10 @@ function Options({ optionType }) {
       imagePath={item?.imagePath}
     />
   ));
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   return <Row>{arr}</Row>;
 }
--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -3,7 +3,7 @@ import { Col } from "react-bootstrap";
 
 const ToppingOption = ({ name, imagePath }) => {
   return (
-    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+    <Col xs={12} sm={6} md={4} lg={2} style={{ textAlign: "center" }}>
       <img
         style={{ width: "75%" }}
         src={`http://localhost:3030/${imagePath}`}
